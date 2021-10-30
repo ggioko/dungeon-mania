e892bdec66f14075bfb95cef402841740beffbd6
Gio Ko <z5310479@ad.unsw.edu.au>
2021-10-30 20:34:23 +1100

    creating intial test

--- src/test/java/dungeonmania/RareCollectibleEntitiesTest.java
+++ src/test/java/dungeonmania/RareCollectibleEntitiesTest.java
@@ -4,22 +4,16 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
 
+import dungeonmania.items.*;
+
 public class RareCollectibleEntitiesTest {
     @Test
-    public void testCharacterHasTheOneRingAfterBattleWin() { // Test if character gets the one ring after winning battle
+    public void testTheOneRing() { // Test if character respawns with full health after getting killed
         DungeonManiaController controller = new DungeonManiaController();
-        controller.newGame("advanced", "standard");
-    }
+        controller.newGame("interact", "standard");
+        controller.currentDungeon.inventory.add(new Item("one_ring_test", "one_ring"));
+        controller.currentDungeon.getPlayer().setHealth(1);
+        
 
-    @Test
-    public void testTheOneRingCharacterRespawn() { // Test if character respawns with full health after getting killed
-        DungeonManiaController controller = new DungeonManiaController();
-        controller.newGame("advanced", "standard");   
-    }
-
-    @Test
-    public void testTheOneRingDiscardAfterUse() { // Test if the one ring discards after one use
-        DungeonManiaController controller = new DungeonManiaController();
-        controller.newGame("advanced", "standard");
     }
 }
