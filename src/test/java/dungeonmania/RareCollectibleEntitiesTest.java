fd9c566bff145e766a813482b18488d4ae01e15d
Gio Ko <z5310479@ad.unsw.edu.au>
2021-10-30 21:00:56 +1100

    Added one ring test

--- src/test/java/dungeonmania/RareCollectibleEntitiesTest.java
+++ src/test/java/dungeonmania/RareCollectibleEntitiesTest.java
@@ -5,6 +5,7 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.junit.jupiter.api.Test;
 
 import dungeonmania.items.*;
+import dungeonmania.util.*;
 
 public class RareCollectibleEntitiesTest {
     @Test
@@ -13,7 +14,14 @@ public class RareCollectibleEntitiesTest {
         controller.newGame("interact", "standard");
         controller.currentDungeon.inventory.add(new Item("one_ring_test", "one_ring"));
         controller.currentDungeon.getPlayer().setHealth(1);
+        controller.tick(null, Direction.DOWN);
+        controller.tick(null, Direction.DOWN);
+        controller.tick(null, Direction.DOWN);
         
-
+        //player runs into hostile entity
+        //Check if player respawns
+        assertTrue(controller.currentDungeon.getPlayer().getHealth() > 95);
+        //Check if one ring is one use
+        assertTrue(controller.currentDungeon.inventory.isEmpty());
     }
 }
