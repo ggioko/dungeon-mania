47152f0152e051b0508365e2926f09c6fd327e36
Neeraj Mirashi <neeraj@192-168-1-187.tpgi.com.au>
2021-11-09 19:49:17 +1100

    created sunStone collectible entity class

---
+++ src/main/java/dungeonmania/entities/collectable/SunStone.java
@@ -0,0 +1,9 @@
+package dungeonmania.entities.collectable;
+
+import org.json.JSONObject;
+
+public class SunStone extends CollectableEntity {
+    public SunStone(JSONObject entity) {
+        super(entity);
+    }
+}
